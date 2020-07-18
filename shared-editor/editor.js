5334c2255000e451e39b5d1aec547fb1ee0209c4
Rogavactive <droga16@freeuni.edu.ge>
2020-07-19 02:59:08 +0400

    bug fix

--- shared-editor/editor.js
+++ shared-editor/editor.js
@@ -1,46 +1,27 @@
+const sharedEditor = {
+    listenEditor: function (editor, endpoint) {
 
-exports.listenEditor = function(editor, endpoint){
+        const socket = new WebSocket(`ws://${endpoint}`);
 
-    const socket = new WebSocket(`ws://${endpoint}`);
-
-    socket.onopen(() => {
-        socket.send('Hello!');
-    });
-
-    socket.onmessage(data => {
-        if(data.action === 'insert'){
-            if(data.start.row !== data.end.row){
-                editor.insert(data.start, '\n')
-            } else {
-                editor.insert(data.start, data.lines[0])
+        socket.addEventListener('message', function (msg) {
+            console.log("data is ");
+            console.log(msg);
+            const data = JSON.parse(msg.data);
+            console.log(data);
+            if (data.action === 'insert') {
+                if (data.start.row !== data.end.row) {
+                    editor.session.insert(data.start, '\n')
+                } else {
+                    editor.session.insert(data.start, data.lines[0])
+                }
+            } else if (data.action === 'remove') {
+                editor.session.replace(new ace.Range(data.start.row, data.start.column, data.end.row, data.end.column), "");
             }
-        } else if (data.action === 'remove'){
-            editor.session.replace(new ace.Range(data.start.row, data.start.column, data.end.row, data.end.column), "");
-        }
-        console.log(data);
-    });
-
-    editor.session.on('change', function(delta) {
-        socket.onopen(() => {
-            socket.send(delta)
         });
-        console.log(delta.start, delta.end, delta.lines, delta.action);
-    });
-};
 
-(function() {
-    window.require(["editor/listenSharedEditor"], function(a) {
-        if (a) {
-            a.config.init(true);
-            a.define = window.define;
-        }
-        if (!window.sharedEditor)
-            window.sharedEditor = a;
-        for (var key in a) if (a.hasOwnProperty(key))
-            window.sharedEditor[key] = a[key];
-        window.sharedEditor["default"] = window.sharedEditor;
-        if (typeof module == "object" && typeof exports == "object" && module) {
-            module.exports = window.sharedEditor;
-        }
-    });
-})();
+        editor.session.on('change', function (delta) {
+            socket.send(JSON.stringify(delta));
+            console.log(delta.start, delta.end, delta.lines, delta.action);
+        });
+    }
+};
