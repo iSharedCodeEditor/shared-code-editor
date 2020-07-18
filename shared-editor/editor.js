2bd9c4d64ce4ec2feae7ed82c50e27d971b1ddbb
Rogavactive <droga16@freeuni.edu.ge>
2020-07-19 01:09:05 +0400

    removed socket.io and started using native js websocket

--- shared-editor/editor.js
+++ shared-editor/editor.js
@@ -1,7 +1,15 @@
 
 exports.listenEditor = function(editor, endpoint){
 
-    var socket = io();
+    const socket = new WebSocket('ws://localhost:3000');
+
+    socket.onopen(() => {
+        socket.send('Hello!');
+    });
+
+    socket.onmessage(data => {
+        console.log(data);
+    });
 
     editor.session.on('change', function(delta) {
         console.log(delta.start, delta.end, delta.lines, delta.action);
