942cd1f7720f06bd86caf98d9236cccbe1666224
Rogavactive <droga16@freeuni.edu.ge>
2020-07-19 12:36:30 +0400

    reworked for usage without op transformation

--- shared-editor/editor.js
+++ shared-editor/editor.js
@@ -1,11 +1,7 @@
 const sharedEditor = {
     debug:false,
-    userFingerPrint: new Date().getTime(),
-    count:0,
     listenEditor: function (editor, endpoint) {
 
-        eventManager.editor = editor;
-        eventManager.startListeningQueue();
         const socket = new WebSocket(`ws://${endpoint}`);
 
         socket.addEventListener('message', function (msg) {
@@ -13,19 +9,11 @@ const sharedEditor = {
             if(sharedEditor.debug){
                 console.log("data received: "+ data);
             }
-            if (data.userFingerPrint === sharedEditor.userFingerPrint){
-
-            }
-            eventManager.enqueueEvent(data);
+            editor.applyChange(data);
         });
 
         editor.session.on('change', function (delta) {
             if (editor.curOp && editor.curOp.command.name){
-                sharedEditor.count++;
-                delta.userFingerprint = sharedEditor.userFingerPrint;
-                delta.timestamp = new Date()
-                delta.count = sharedEditor.count;
-                console.log(sharedEditor.debug);
                 if (sharedEditor.debug){
                     console.log("change by current user", delta);
                     console.log(delta.start, delta.end, delta.lines, delta.action);
@@ -37,22 +25,6 @@ const sharedEditor = {
                 }
             }
         });
-    }
-};
-
-const eventManager = {
-    editor: undefined,
-    newEventQueue: [], //queue for incoming events to add it in historyQueue
-    historyQueue: [], //queue for storing history, for operational transformation.
-    startListeningQueue(){
-        setInterval(()=>{
-            while(eventManager.newEventQueue.length > 0){
-                var data = eventManager.newEventQueue.shift();
-                if (data.timestamp > eventManager.historyQueue[eventManager.historyQueue.length-1]){
-                    eventManager.applyChange(data);
-                }
-            }
-        },10);
     },
     applyChange(data){
         if (data.action === 'insert') {
@@ -64,12 +36,5 @@ const eventManager = {
         } else if (data.action === 'remove') {
             editor.session.replace(new ace.Range(data.start.row, data.start.column, data.end.row, data.end.column), "");
         }
-    },
-    enqueueEvent(data){
-        eventManager.newEventQueue.push(data);
     }
-};
-
-transformator = {
-
 };
