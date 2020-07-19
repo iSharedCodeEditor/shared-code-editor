3959dfaf5efda34d0dae872054438269d0e4d936
negnatashvili <negnatashvili@bog.ge>
2020-07-19 14:09:03 +0400

    Added function to get text from user

--- demo/index.js
+++ demo/index.js
@@ -1,14 +1,29 @@
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 4200 });
+var text = 'Hello world';
 
 wss.on('connection', function connection(ws) {
+    ws.send(JSON.stringify({
+        start:{row:0, col:0},
+        end:{row:0, col:0},
+        action:"insert",
+        lines: [text]
+    }));
     ws.on('message', function incoming(data) {
         console.log(data);
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data);
-            }
-        });
+        dataObj = JSON.parse(data);
+        if(dataObj.hasOwnProperty('getText')){
+            text = dataObj['getText']
+        } else {
+            wss.clients.forEach(function each(client) {
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    client.send(data);
+                }
+            });
+        }
     });
+    setInterval( ()=>{
+        ws.send(JSON.stringify({getText:""}))
+    }, 1000)
 });
--- shared-editor/editor.js
+++ shared-editor/editor.js
@@ -1,39 +1,44 @@
 const sharedEditor = {
-    debug: false,
+    debug:false,
     listenEditor: function (editor, endpoint) {
 
         const socket = new WebSocket(`ws://${endpoint}`);
 
         socket.addEventListener('message', function (msg) {
             const data = JSON.parse(msg.data);
-            if (sharedEditor.debug) {
-                console.log("data received: " + data);
+            if(sharedEditor.debug){
+                console.log("data received: "+ data);
             }
-            sharedEditor.applyChange(data);
+            if(data.hasOwnProperty('getText')){
+                socket.send(JSON.stringify({
+                    getText:editor.getValue()
+                }))
+            } else {
+                sharedEditor.applyChange(data);
+            }
         });
 
         editor.session.on('change', function (delta) {
-            if (editor.curOp && editor.curOp.command.name) {
-                if (sharedEditor.debug) {
+            if (editor.curOp && editor.curOp.command.name){
+                if (sharedEditor.debug){
                     console.log("change by current user", delta);
                     console.log(delta.start, delta.end, delta.lines, delta.action);
                 }
                 socket.send(JSON.stringify(delta));
             } else {
-                if (sharedEditor.debug) {
+                if (sharedEditor.debug){
                     console.log("change by other user", delta);
                 }
             }
         });
     },
-    applyChange(data) {
+    applyChange(data){
         if (data.action === 'insert') {
             let dataStr = "";
             var i;
             for (i = 0; i < data.lines.length; i++) {
                 dataStr += data.lines[i] + '\n';
             }
-            dataStr = dataStr.substr(0, dataStr.length - 1);
             editor.session.insert(data.start, dataStr)
         } else if (data.action === 'remove') {
             editor.session.replace(new ace.Range(data.start.row, data.start.column, data.end.row, data.end.column), "");
