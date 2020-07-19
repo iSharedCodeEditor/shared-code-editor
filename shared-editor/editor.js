64483552f90c55ee49f37db0b36ecf4366be0629
Rogavactive <droga16@freeuni.edu.ge>
2020-07-19 13:04:21 +0400

    fix issue

--- shared-editor/editor.js
+++ shared-editor/editor.js
@@ -1,38 +1,39 @@
 const sharedEditor = {
-    debug:false,
+    debug: false,
     listenEditor: function (editor, endpoint) {
 
         const socket = new WebSocket(`ws://${endpoint}`);
 
         socket.addEventListener('message', function (msg) {
             const data = JSON.parse(msg.data);
-            if(sharedEditor.debug){
-                console.log("data received: "+ data);
+            if (sharedEditor.debug) {
+                console.log("data received: " + data);
             }
-            editor.applyChange(data);
+            sharedEditor.applyChange(data);
         });
 
         editor.session.on('change', function (delta) {
-            if (editor.curOp && editor.curOp.command.name){
-                if (sharedEditor.debug){
+            if (editor.curOp && editor.curOp.command.name) {
+                if (sharedEditor.debug) {
                     console.log("change by current user", delta);
                     console.log(delta.start, delta.end, delta.lines, delta.action);
                 }
                 socket.send(JSON.stringify(delta));
             } else {
-                if (sharedEditor.debug){
+                if (sharedEditor.debug) {
                     console.log("change by other user", delta);
                 }
             }
         });
     },
-    applyChange(data){
+    applyChange(data) {
         if (data.action === 'insert') {
             let dataStr = "";
             var i;
             for (i = 0; i < data.lines.length; i++) {
                 dataStr += data.lines[i] + '\n';
             }
+            dataStr = dataStr.substr(0, dataStr.length - 1);
             editor.session.insert(data.start, dataStr)
         } else if (data.action === 'remove') {
             editor.session.replace(new ace.Range(data.start.row, data.start.column, data.end.row, data.end.column), "");
